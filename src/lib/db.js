1f96e4d0bd4c6504bb253052e6f6413740d7de9f
joikoi1212 <a.morais0101@gmail.com>
2025-02-21 12:19:43 +0000

    commit

--- src/lib/db.js
+++ src/lib/db.js
@@ -10,10 +10,11 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0,
     connectTimeout: 10000,
-    ssl: {
+   /* ssl: {
   ca: Buffer.from(process.env.DB_SSL_CA_BASE64, 'base64').toString('utf-8'),
 
     }, 
+    */
     
 });
 
